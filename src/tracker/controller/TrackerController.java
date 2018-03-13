3562d4b71d3495f6d96f3113b765abe6b7b15334
Ben Charlesworth <bencharlesworth4@gmail.com>
2018-03-13 13:42:05 -0600

    Added setters and getters to the Media object and tested sorting.

--- src/tracker/controller/TrackerController.java
+++ src/tracker/controller/TrackerController.java
@@ -1,13 +1,39 @@
 package tracker.controller;
 
 import tracker.view.TrackerFrame;
+import tracker.model.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class TrackerController
 {
 	private TrackerFrame appFrame;
-	
+	private List<Media> media;
+
 	public TrackerController()
 	{
 		appFrame = new TrackerFrame(this);
+		media = new ArrayList<Media>();
+		for (int i = 0; i < 100000; i++)
+		{
+			media.add(new Game(null, null, null, null, null, (int) (Math.random() * 200000)));
+		}
+		for (int i = 0; i < 100000; i++)
+		{
+			media.add(new Book(null, null, null, null, null, (int) (Math.random() * 200000)));
+		}
+		for (int i = 0; i < 100000; i++)
+		{
+			media.add(new Movie(null, null, null, null, null, (int) (Math.random() * 200000)));
+		}
+		for (int i = 0; i < 100000; i++)
+		{
+			media.add(new Show(null, null, null, null, null, (int) (Math.random() * 200000)));
+		}
+		media.sort((obj1, obj2) -> obj1.getRating() - obj2.getRating());
+		for (Media med : media)
+		{
+			System.out.println(med.getRating());
+		}
 	}
 }
--- src/tracker/model/Media.java
+++ src/tracker/model/Media.java
@@ -19,4 +19,100 @@ public abstract class Media implements Serializable
 		this.endDate = endDate;
 		this.rating = rating;
 	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle()
+	{
+		return title;
+	}
+
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public String getType()
+	{
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type)
+	{
+		this.type = type;
+	}
+
+	/**
+	 * @return the genres
+	 */
+	public String[] getGenres()
+	{
+		return genres;
+	}
+
+	/**
+	 * @param genres the genres to set
+	 */
+	public void setGenres(String[] genres)
+	{
+		this.genres = genres;
+	}
+
+	/**
+	 * @return the startDate
+	 */
+	public Date getStartDate()
+	{
+		return startDate;
+	}
+
+	/**
+	 * @param startDate the startDate to set
+	 */
+	public void setStartDate(Date startDate)
+	{
+		this.startDate = startDate;
+	}
+
+	/**
+	 * @return the endDate
+	 */
+	public Date getEndDate()
+	{
+		return endDate;
+	}
+
+	/**
+	 * @param endDate the endDate to set
+	 */
+	public void setEndDate(Date endDate)
+	{
+		this.endDate = endDate;
+	}
+
+	/**
+	 * @return the rating
+	 */
+	public int getRating()
+	{
+		return rating;
+	}
+
+	/**
+	 * @param rating the rating to set
+	 */
+	public void setRating(int rating)
+	{
+		this.rating = rating;
+	}
 }
